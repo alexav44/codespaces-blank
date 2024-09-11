edb333beb5b497f311c4022b99032b14ba863294
alexav44 <alexav44@nycstudents.net>
2024-09-11 02:23:56 +0000

    first commit

---
+++ Homework2.java
@@ -0,0 +1,18 @@
+public class Homework2
+{ 
+    public static void main(String[] args)
+    {
+        int w = 13;
+        final double c = 2.50;
+        String l = (" lbs should eat ");
+        System.out.println("cats " + w + l + c + " cans");
+        w = 14;
+        double s =3.5;
+        s = w + s ;
+        boolean y = true;
+        System.out.println("cats " + s + l + "much less");
+        System.out.println("Fat cats are not cute" );
+        System.out.print("It's "+y);
+    }
+
+}
